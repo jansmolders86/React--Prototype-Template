6c4e9b17ed3cdf9c835e70b33b7ecd6ea153d68b
James-Jensen <James.Jensen@epicgames.com>
2018-01-22 14:29:41 -0500

    First commit for React Prototype

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+//import {Switch, Route} from 'react-router';
+import './App.sass';
+
+class App extends Component {
+    render() {
+        return (
+            <div id="prototype">
+                {/*prototype code will go in here*/}
+            </div>
+        );
+    }
+}
+
+export default App;
